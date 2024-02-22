14022b470804e0be280325a0d41ba3aa4c82aa76
AlphaMoonbaseBerlin <kontakt@alphamoonbase.de>
2024-02-22 10:34:31 +0100

    Updated Init and Config Component.

--- Modules/suspects/project/Configs/GeneralConfig_callbacks.py
+++ Modules/suspects/project/Configs/GeneralConfig_callbacks.py
@@ -2,39 +2,38 @@
 Name : GeneralConfig_callbacks
 Author : Wieland@AMB-ZEPH15
 Saveorigin : Project.toe
-Saveversion : 2022.32660
+Saveversion : 2022.35320
 Info Header End'''
-from typing import Type
 
-def GetConfigSchema(configValue:Type["ConfigValue"], collectionDict:Type["CollectionDict"], collectionList:Type["CollectionList"]) -> dict:
-
-	positiveValue = configValue(default = 4, validator = lambda value : value > 0)
+def GetConfigSchema(configModule:"SchemaObjects", configComp:"JsonConfig") -> dict:
+	positiveValue = configModule.ConfigValue(default = 4, validator = lambda value : value > 0)
 	
 	return {
-    	"Some_Integer" : configValue( 
+		"NamedList" : configModule.NamedList(),
+    	"Some_Integer" : configModule.ConfigValue( 
 			default = -2, 
 			validator = lambda value: value < 4,
 			typecheck = (float, int)
 		 ),
-    	"Some_String"   : configValue( default = "foo", validator= lambda value: isinstance( value, str)),
-    	"Subdata" : collectionDict({
-        	"Sub" : configValue( default = 13, validator= lambda value: value < 14 ),
-			"SubDict" : collectionDict({
-				"SubSubValue" : configValue( 
+    	"Some_String"   : configModule.ConfigValue( default = "foo", validator= lambda value: isinstance( value, str)),
+    	"Subdata" : configModule.CollectionDict({
+        	"Sub" : configModule.ConfigValue( default = 13, validator= lambda value: value < 14 ),
+			"SubDict" : configModule.CollectionDict({
+				"SubSubValue" : configModule.ConfigValue( 
 					default = "Foobar",
 					typecheck = str,
 					parser = lambda value: value.upper()
 				)
 			})
     		}),
-    	"Sublist" : collectionList( ),
-	    "Prefilled_Sublist" : collectionList(
+    	"Sublist" : configModule.CollectionList( ),
+	    "Prefilled_Sublist" : configModule.CollectionList(
 			["Hello", "WOrld"]
 		),
 		"PositiveValue" : positiveValue(),
-		"Prefilled_Validated" : collectionList(
+		"Prefilled_Validated" : configModule.CollectionList(
 			items = [1,2,3],
-			default_member = configValue(default = -1, validator = lambda value: value < 3)
+			default_member = configModule.ConfigValue(default = -1, validator = lambda value: value < 3)
 		)
 		}
 		
